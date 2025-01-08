234e8bf244ef395663dd395f732329b463fbe05b
Redume <danilivasenko09@gmail.com>
2025-01-09 00:18:24 +0300

    fix: handle exponential notation and large numbers in format_number function

--- utils/format_number.py
+++ utils/format_number.py
@@ -1,14 +1,12 @@
+from decimal import Decimal
+
 def format_number(number):
-    number_str = str(number)
-
-    if '.' in number_str:
-        integer_part, fractional_part = number_str.split('.')
-    else:
-        integer_part, fractional_part = number_str, ''
-
-    formatted_integer_part = '{:,}'.format(int(integer_part)).replace(',', ' ')
-
-    if fractional_part:
+    number = Decimal(str(number))
+    
+    formatted_integer_part = '{:,.0f}'.format(number).replace(',', ' ')
+    
+    if '.' in str(number):
+        fractional_part = str(number).split('.')[1]
         return formatted_integer_part + '.' + fractional_part
     else:
         return formatted_integer_part
