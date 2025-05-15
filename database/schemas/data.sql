b5d8edcc38bfd22ee9e5af0df31f73b33ff5dfa8
Redume <danilivasenko09@gmail.com>
2025-05-15 14:53:52 +0300

    chore: added a new field to customize the schedule period

--- database/schemas/data.sql
+++ database/schemas/data.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER UNIQUE NOT NULL,
+    user_id INTEGER PRIMARY KEY,
     chart BOOLEAN DEFAULT 1,
+    chart_period TEXT DEFAULT 'month',
     lang TEXT DEFAULT 'en'
 );
