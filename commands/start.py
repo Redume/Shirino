1ae3ec9aec831a495c6f7ccf8a8bb42118208b8e
Redume <danilivasenko09@gmail.com>
2025-05-15 15:06:40 +0300

    chore: the link in the post has been replaced with a button

--- commands/start.py
+++ commands/start.py
@@ -8,11 +8,21 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: types.Message) -> None:
     get_bot = await bot.get_me()
+
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=[
+        [
+            types.InlineKeyboardButton(
+                text="Source Code",
+                url="https://github.com/redume/shirino"
+                )
+        ]
+    ])
+
     await message.reply(
         'Shirino is a telegram bot for converting fiat or cryptocurrency. '
         'The example of use occurs via inline query:\n'
         f'`@{get_bot.username} USD RUB` \n'
-        f'`@{get_bot.username} 12 USD RUB` \n\n'
-        '[Source Code](https://github.com/Redume/Shirino)',
-        parse_mode='markdown'
+        f'`@{get_bot.username} 12 USD RUB` \n\n',
+        parse_mode='markdown',
+        reply_markup=keyboard
     )
