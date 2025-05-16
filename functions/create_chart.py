5fe0a5ae3a76fbbb4eaf9641aa455993e9449416
Redume <danilivasenko09@gmail.com>
2025-05-17 00:35:27 +0300

    chore: added a new field for period customization

--- functions/create_chart.py
+++ functions/create_chart.py
@@ -5,10 +5,16 @@ import aiohttp
 
 config = yaml.safe_load(open('../config.yaml', 'r', encoding='utf-8'))
 
-async def create_chart(from_currency: str, conv_currency: str) -> (dict, None):
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=3)) as session:
+async def create_chart(
+    from_currency: str,
+    conv_currency: str,
+    period: str) -> (dict, None):
+    async with aiohttp.ClientSession(
+        timeout=aiohttp.ClientTimeout(total=3)
+        ) as session:
         async with session.get(
-            f'{config["kekkai_instance"]}/api/getChart/month/', params={
+            f'{config["kekkai_instance"]}/api/getChart/{period}/',
+            params={
                 'from_currency': from_currency,
                 'conv_currency': conv_currency
                 }) as res:
