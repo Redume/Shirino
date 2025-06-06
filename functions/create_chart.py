81a57e4b9eefaebf714075e2ffb5422fff6d0026
Redume <danilivasenko09@gmail.com>
2025-06-06 11:19:42 +0300

    chore: Transmitted the receipt of the graph

--- functions/create_chart.py
+++ functions/create_chart.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from urllib.parse import urlencode
 
 import yaml
 import aiohttp
@@ -8,19 +9,22 @@ config = yaml.safe_load(open('../config.yaml', 'r', encoding='utf-8'))
 async def create_chart(
     from_currency: str,
     conv_currency: str,
-    period: str) -> (dict, None):
-    async with aiohttp.ClientSession(
-        timeout=aiohttp.ClientTimeout(total=3)
-        ) as session:
-        async with session.get(
-            f'{config["kekkai_instance"]}/api/getChart/{period}/',
-            params={
-                'from_currency': from_currency,
-                'conv_currency': conv_currency
-                }) as res:
+    period: str,
+    backend: str) -> (str, None):
+    params = {
+        'from_currency': from_currency,
+        'conv_currency': conv_currency,
+        'period': period,
+        'backend': backend
+    }
+    
+    base_url = f'{config["kekkai_instance"]}/api/getChart/'
+    query_string = urlencode(params)
+    full_url = f'{base_url}?{query_string}'
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=3)) as session:
+        async with session.get(full_url) as res:
             if not HTTPStatus(res.status).is_success:
                 return None
-
-            data = await res.json()
-
-            return data.get('detail', None)
+    
+            return full_url
